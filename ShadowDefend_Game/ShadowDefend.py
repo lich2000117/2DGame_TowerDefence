27f9612001911a224ab19d71c7f429a6d97e1328
Chenghao Li <lich2000117@gmail.com>
2021-02-22 13:36:18 +0800

    now can run mac app

--- ShadowDefend_Game/ShadowDefend.py
+++ ShadowDefend_Game/ShadowDefend.py
@@ -2,7 +2,6 @@ import subprocess
 import sys
 import os
 import time
-#subprocess.call(['java', '-jar', 'bagel.jar'])
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -16,8 +15,6 @@ def resource_path(relative_path):
 
 def run_game(file_path):
     file_path = resource_path(file_path)
-    print(file_path)
-    input("press to continue")
     subprocess.call(['java', '-jar', file_path])
 
 if __name__ == '__main__':
